8c9e961a07fe1fe5c6fdd08cdec00708ea7cde29
Shubham4322 <shubhambhingude4846@gmail.com>
2026-07-04 15:31:25 +0530

    Time: 0 ms (100%), Space: 19.3 MB (65.53%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        b = {'{':'}','(':')','[':']'}
+        c =[] 
+        for i in s:
+            if i not in b:
+                if not(c) or c.pop()!=i:
+                    return False
+            else:
+                c.append(b[i])
+        return len(c)==0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # b = {'(':')','{':'}','[':']'}
+        # c =[]
+        # for i in s:
+        #     if i not in b:
+        #         if not(c) or c.pop()!=i:
+        #             return False
+        #     else:
+        #         c.append(b[i])
+        # return len(c)==0
