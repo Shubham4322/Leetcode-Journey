cfa0add2dadb213b7cae55193f8fc0ac32a5d813
Shubham4322 <shubhambhingude4846@gmail.com>
2026-07-06 23:13:51 +0530

    Time: 0 ms (100%), Space: 19.1 MB (87.78%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -11,9 +11,8 @@ class Solution:
 
         for i in range(len(digits)-1,-1,-1):
             if digits[i]<9:
-                digits[i]+=1
+                digits[i]=digits[i]+1
                 return digits
-
-            else:
+            elif digits[i] == 9:
                 digits[i]=0
-        return [1]+digits
+        return [1]+digits 
